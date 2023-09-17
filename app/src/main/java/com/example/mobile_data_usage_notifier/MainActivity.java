eaa99e60dce8d64d9aa67c3db3c984a330bfce6c
Yasin Maden <maden.yasin@hotmail.com>
2023-09-17 23:51:40 +0300

    Bildirimler için temel oluşturuldu.

--- app/src/main/java/com/example/mobile_data_usage_notifier/MainActivity.java
+++ app/src/main/java/com/example/mobile_data_usage_notifier/MainActivity.java
@@ -3,28 +3,48 @@ package com.example.mobile_data_usage_notifier;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_NOTIFICATION_PERMISSION = 155;
+    private static final String CHANNEL_ID = "data_usage";
+    private static final int NOTIFICATION_ID = 112;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.button);
         if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
             // izin verilmiş
         } else {
             // izin veilmemiş, izin isteniyor
             requestNotificationPermission();
         }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManagerCompat.from(MainActivity.this).createNotificationChannel(createNotificationChannel());
+
+                sendNotification();
+            }
+        });
     }
+
 
     private void requestNotificationPermission() {
         if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
@@ -46,5 +66,33 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Permission denied. You cannot send notifications.", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private NotificationChannel createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Mobile data usage alert notifications";
+            String description = "It comes into play when mobile data is used effectively.";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            return channel;
+        }
+
+        return null;
+    }
+    @SuppressLint("MissingPermission")
+    private void sendNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Mobile Data Usage Warning!")
+                .setContentText("Mobile data is used effectively. Please turn off mobile data and connect to a WI-FI point.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
